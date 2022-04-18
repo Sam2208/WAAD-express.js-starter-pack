35383ac90e0cc26f1987a5271bd545510eeb686d
SamuelM <56120698+Sam2208@users.noreply.github.com>
2022-04-18 22:30:04 +0200

    3. http-codes

---
+++ http-codes/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const routes = require('./routes');
+
+
+// routes
+app.use('/', routes);
+
+// 404 Not Found
+app.all('*', (req, res) => {
+            res.status(404).send({message : "This resource was not found"})
+    })
+    
+app.listen(3000, () => console.info('Application running on port 3000'));
